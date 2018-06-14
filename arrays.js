8c928f098ad71045d5e05cf3914536328d384881
Matt Nicholas <fostermatt82@gmail.com>
2018-06-14 19:15:19 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,15 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+
+function addElementToBeginningOfArray(array){
+  return;
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array){
+  return array.shift();
+}
+
+function removeElementFromBeginningOfArray(array){
+  return array.slice();
+}
+
+function destructivelyRemoveElementFromEndOfArray 
